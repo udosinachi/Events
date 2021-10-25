e67201841b23548c34b1091751e9b057d85b7206
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-10-25 21:47:17 +0100

    Completed the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,34 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='footer text'>@udosinachi</div>
+      <div className='footer-div'>
+        <div className='footer-logo'>
+          <img
+            src='https://branchoutsf.com/img/logo/logo.leaf.png'
+            alt='branchout'
+          />
+        </div>
+        <div className='footer-icons'>
+          <i className='fab fa-twitter-square'></i>
+          <i className='fab fa-facebook-square'></i>
+        </div>
+
+        <div className='footer-links'>
+          <a href='lik'>About Us</a>
+          <a href='lik'>Terms of use</a>
+          <a href='lik'>Privacy</a>
+        </div>
+      </div>
+
+      <div className='footer-copyrights'>
+        <span>
+          Branch provides the leading mobile linking platform with solutions
+          that unify user experience and measurement across different devices,
+          platforms, and channels.
+        </span>
+        <br />
+        <span>©2017 Branch. All rights reserved.</span>
+      </div>
     </div>
   )
 }
