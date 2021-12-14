23761061704349bbe6e6b32f952a3d8c11d06685
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-12-14 18:47:38 +0100

    Styling

--- src/pages/showmore/Showmore.js
+++ src/pages/showmore/Showmore.js
@@ -28,33 +28,35 @@ const Showmore = () => {
     <div className='showmore'>
       <Navbar />
       <div className='showmore-div'>
-        {loader === true ? (
-          <div className='loader'>
-            <CircularProgress className='main-loader' />
-          </div>
-        ) : (
-          <>
-            {cardName.map((list) => {
-              return (
-                <div className='main-showmore' key={list._id}>
-                  <Link to={`/profile/${list._id}`} className='a'>
-                    <div className='showmore-image'>
-                      <div className='showmore-image-subdiv'>
-                        <img src={list.image} alt='events' className='img' />
+        <div className='showmore-subdiv'>
+          {loader === true ? (
+            <div className='loader'>
+              <CircularProgress className='main-loader' />
+            </div>
+          ) : (
+            <>
+              {cardName.map((list) => {
+                return (
+                  <div className='main-showmore' key={list._id}>
+                    <Link to={`/profile/${list._id}`} className='a'>
+                      <div className='showmore-image'>
+                        <div className='showmore-image-subdiv'>
+                          <img src={list.image} alt='events' className='img' />
+                        </div>
                       </div>
-                    </div>
-                    <div className='showmore-content'>
-                      <p className='showmore-p'>{list.businessName}</p>
-                      <h4 className='showmore-h4'>{list.email}</h4>
-                      <p className='showmore-p2'>{list.category}</p>
-                      <p className='showmore-p3'>{list.userText}</p>
-                    </div>
-                  </Link>
-                </div>
-              )
-            })}
-          </>
-        )}
+                      <div className='showmore-content'>
+                        <p className='showmore-p'>{list.businessName}</p>
+                        <h4 className='showmore-h4'>{list.email}</h4>
+                        <p className='showmore-p2'>{list.category}</p>
+                        <p className='showmore-p3'>{list.userText}</p>
+                      </div>
+                    </Link>
+                  </div>
+                )
+              })}
+            </>
+          )}
+        </div>
       </div>
       <Footer />
     </div>
