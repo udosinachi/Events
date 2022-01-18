7e88a24998ec978a0e5967e17196f56fe0acddaa
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2022-01-18 09:23:52 +0100

    Get updates is partially set

--- src/components/updates/Updates.js
+++ src/components/updates/Updates.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import './Update.css'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Updates = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    toast.success('Opted for Updates')
+    setLastName('')
+    setFirstName('')
+    setEmail('')
+  }
   return (
-    <div className='update'>
-      <h3 className='update-text'>Get Updates for New Posts</h3>
-      <div className='update-input'>
-        <div className='update-input-div'>
-          <input type='text' placeholder='First Name' />
+    <form onSubmit={handleSubmit}>
+      <div className='update'>
+        <h3 className='update-text'>Get Updates for New Posts</h3>
+        <div className='update-input'>
+          <div className='update-input-div'>
+            <input
+              type='text'
+              placeholder='First Name'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className='update-input-div'>
+            <input
+              type='text'
+              placeholder='Last Name'
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className='update-input-div'>
+            <input
+              type='text'
+              placeholder='Email Address'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
         </div>
-        <div className='update-input-div'>
-          <input type='text' placeholder='Last Name' />
-        </div>
-        <div className='update-input-div'>
-          <input type='text' placeholder='Email Address' />
-        </div>
+        <button className='update-button' type='submit'>
+          Get Updates
+        </button>
       </div>
-      <button className='update-button'>Get Updates</button>
-    </div>
+    </form>
   )
 }
 
