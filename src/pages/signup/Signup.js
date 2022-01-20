dbb217d4e85a89483fed4e32288200bcd823f32f
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2022-01-20 20:35:13 +0100

    Description tab

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -21,6 +21,7 @@ import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import CircularProgress from '@mui/material/CircularProgress'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
+// import TextareaAutosize from '@mui/material/TextareaAutosize'
 
 const theme = createTheme()
 
@@ -34,8 +35,10 @@ export default function Signup() {
   const [phoneNumber, setPhoneNumber] = useState('')
   const [password, setPassword] = useState('')
   const [category, setCategory] = useState('')
+  const [userText, setUserText] = useState('')
   const [career, setCareer] = useState([])
   const [eye, setEye] = useState(true)
+  const [description, setDescription] = useState(true)
 
   useEffect(() => {
     axios
@@ -57,6 +60,7 @@ export default function Signup() {
       email,
       password,
       category,
+      userText,
     }
     setLoader(true)
     axios
@@ -70,6 +74,7 @@ export default function Signup() {
           setEmail('')
           setPassword('')
           setCategory('')
+          setUserText('')
           localStorage.setItem('token', res.data.user.token)
           localStorage.setItem('id', res.data.user._id)
           localStorage.setItem('fullName', res.data.user.fullName)
@@ -79,6 +84,7 @@ export default function Signup() {
           localStorage.setItem('password', res.data.user.password)
           localStorage.setItem('category', res.data.user.category)
           localStorage.setItem('image', res.data.user.image)
+          localStorage.setItem('userText', res.data.user.userText)
           setLoader(false)
           window.setTimeout(() => {
             history.push('/')
@@ -90,186 +96,307 @@ export default function Signup() {
       })
   }
 
+  const nextClick = (e) => {
+    e.preventDefault()
+
+    // if (
+    //   !fullName ||
+    //   !businessName ||
+    //   !email ||
+    //   !phoneNumber ||
+    //   !password ||
+    //   !category
+    // ) {
+    //   toast.error('all fields must be entered')
+    // } else {
+    //   setDescription(false)
+    // }
+
+    setDescription(false)
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component='main' sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light'
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: '#20364b' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component='h1' variant='h5'>
-              Sign Up
-            </Typography>
-            <Box
-              component='form'
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+    <div>
+      {description === true ? (
+        <ThemeProvider theme={theme}>
+          <Grid container component='main' sx={{ height: '100vh' }}>
+            <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={4}
+              md={7}
+              sx={{
+                backgroundImage: 'url(https://source.unsplash.com/random)',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: (t) =>
+                  t.palette.mode === 'light'
+                    ? t.palette.grey[50]
+                    : t.palette.grey[900],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={5}
+              component={Paper}
+              elevation={6}
+              square
             >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete='fname'
-                    name='fullName'
-                    required
-                    fullWidth
-                    id='fullName'
-                    label='Full Name'
-                    autoFocus
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id='businessName'
-                    label='Business Name'
-                    name='businessName'
-                    autoComplete='lname'
-                    value={businessName}
-                    onChange={(e) => setBusinessName(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id='email'
-                    label='Email Address'
-                    name='email'
-                    autoComplete='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id='phoneNumber'
-                    label='Phone Number'
-                    name='phoneNumber'
-                    autoComplete='pnumber'
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12} className='password-grid'>
-                  <TextField
-                    required
-                    fullWidth
-                    name='password'
-                    label='Password'
-                    type={eye ? 'password' : 'text'}
-                    id='password'
-                    autoComplete='new-password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {eye ? (
-                    <Visibility
-                      className='password-icon'
-                      onClick={() => setEye(false)}
-                    />
-                  ) : (
-                    <VisibilityOff
-                      className='password-icon'
-                      onClick={() => setEye(true)}
-                    />
-                  )}
-                </Grid>
-
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <InputLabel id='demo-simple-select-label'>
-                      Category *
-                    </InputLabel>
-
-                    <Select
-                      labelId='demo-simple-select-label'
-                      id='demo-simple-select'
-                      label='Category *'
-                      onChange={(e) => setCategory(e.target.value)}
-                      value={category}
-                    >
-                      {career.map((cat) => (
-                        <MenuItem key={cat._id} value={cat.name}>
-                          {cat.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value='allowExtraEmails' color='primary' />
-                    }
-                    label='I want to receive updates via email.'
-                  />
-                </Grid>
-              </Grid>
-              {loader === false ? (
-                <Button
-                  type='submit'
-                  fullWidth
-                  variant='contained'
-                  sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
-                >
+              <Box
+                sx={{
+                  my: 8,
+                  mx: 4,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: '#20364b' }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component='h1' variant='h5'>
                   Sign Up
-                </Button>
-              ) : (
-                <Button
-                  type='submit'
-                  fullWidth
-                  variant='contained'
-                  sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
+                </Typography>
+                <Box
+                  component='form'
+                  noValidate
+                  // onSubmit={handleSubmit}
+                  sx={{ mt: 1 }}
                 >
-                  <CircularProgress />
-                </Button>
-              )}
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        autoComplete='fname'
+                        name='fullName'
+                        required
+                        fullWidth
+                        id='fullName'
+                        label='Full Name'
+                        autoFocus
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        fullWidth
+                        id='businessName'
+                        label='Business Name'
+                        name='businessName'
+                        autoComplete='lname'
+                        value={businessName}
+                        onChange={(e) => setBusinessName(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id='email'
+                        label='Email Address'
+                        name='email'
+                        autoComplete='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id='phoneNumber'
+                        label='Phone Number'
+                        name='phoneNumber'
+                        autoComplete='pnumber'
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12} className='password-grid'>
+                      <TextField
+                        required
+                        fullWidth
+                        name='password'
+                        label='Password'
+                        type={eye ? 'password' : 'text'}
+                        id='password'
+                        autoComplete='new-password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      {eye ? (
+                        <Visibility
+                          className='password-icon'
+                          onClick={() => setEye(false)}
+                        />
+                      ) : (
+                        <VisibilityOff
+                          className='password-icon'
+                          onClick={() => setEye(true)}
+                        />
+                      )}
+                    </Grid>
 
-              <Grid container justifyContent='flex-end'>
-                <Grid item>
-                  <Link to='login' variant='body2' className='link'>
-                    Already have an account? Sign In
-                  </Link>
-                </Grid>
-              </Grid>
-              <div sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-      <ToastContainer />
-    </ThemeProvider>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <InputLabel id='demo-simple-select-label'>
+                          Category *
+                        </InputLabel>
+
+                        <Select
+                          labelId='demo-simple-select-label'
+                          id='demo-simple-select'
+                          label='Category *'
+                          onChange={(e) => setCategory(e.target.value)}
+                          value={category}
+                        >
+                          {career.map((cat) => (
+                            <MenuItem key={cat._id} value={cat.name}>
+                              {cat.name}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox value='allowExtraEmails' color='primary' />
+                        }
+                        label='I want to receive updates via email.'
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
+                    onClick={(e) => nextClick(e)}
+                  >
+                    Next
+                  </Button>
+
+                  <Grid container justifyContent='flex-end'>
+                    <Grid item>
+                      <Link to='login' variant='body2' className='link'>
+                        Already have an account? Sign In
+                      </Link>
+                    </Grid>
+                  </Grid>
+                  <div sx={{ mt: 5 }} />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+          <ToastContainer />
+        </ThemeProvider>
+      ) : (
+        <ThemeProvider theme={theme}>
+          <Grid container component='main' sx={{ height: '100vh' }}>
+            <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={4}
+              md={7}
+              sx={{
+                backgroundImage: 'url(https://source.unsplash.com/random)',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: (t) =>
+                  t.palette.mode === 'light'
+                    ? t.palette.grey[50]
+                    : t.palette.grey[900],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={5}
+              component={Paper}
+              elevation={6}
+              square
+            >
+              <Box
+                sx={{
+                  my: 8,
+                  mx: 4,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: '#20364b' }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component='h1' variant='h5'>
+                  Company's Description
+                </Typography>
+                <Box
+                  component='form'
+                  noValidate
+                  onSubmit={handleSubmit}
+                  sx={{ mt: 1 }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        autoComplete='usertext'
+                        name='userText'
+                        required
+                        fullWidth
+                        id='userText'
+                        label="Company's Description"
+                        autoFocus
+                        value={userText}
+                        onChange={(e) => setUserText(e.target.value)}
+                      />
+                    </Grid>
+                  </Grid>
+
+                  <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
+                  >
+                    Sign Up
+                  </Button>
+                  {/* {loader === true ? (
+                    <Button
+                      type='submit'
+                      fullWidth
+                      variant='contained'
+                      sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
+                    >
+                      Sign Up
+                    </Button>
+                  ) : (
+                    <Button
+                      type='submit'
+                      fullWidth
+                      variant='contained'
+                      sx={{ mt: 3, mb: 2, bgcolor: '#20364b' }}
+                    >
+                      <CircularProgress />
+                    </Button>
+                  )} */}
+                  <div sx={{ mt: 5 }} />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+          <ToastContainer />
+        </ThemeProvider>
+      )}
+    </div>
   )
 }
